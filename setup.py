d771ec3b7839710622dbb4b8984a7c899e2eafdf
Matthijs Mars <matthijs.mars97@gmail.com>
2020-10-27 16:59:47 +0100

    quick sparse reconstruction with a simple measurement operator

---
+++ setup.py
@@ -0,0 +1,11 @@
+  
+from setuptools import find_packages, setup
+
+setup(
+    name='aiai',
+    packages=find_packages(),
+    version='0.1.0',
+    description='AI for imaging inverse problems',
+    author='Matthijs Mars',
+    # license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
+)
---
+++ src/operators.py
@@ -0,0 +1,92 @@
+
+import numpy as np
+from pynufft import NUFFT
+
+import optimusprimal.primal_dual as primal_dual
+import optimusprimal.linear_operators as linear_operators
+import optimusprimal.prox_operators as prox_operators
+
+
+class MeasurementOperator():
+    """Simple measurement operator using the pyNUFFT package to sample at non-uniform u,v coordinates"""
+    def __init__(self, vis, Nd=(256,256), Kd=(512,512), Jd=(6,6)):
+        """ Initialises the measurement operators for u,v coordinates 'vis' (M x 2)"""
+        self.op = NUFFT()
+        self.op.plan(vis, Nd, Kd, Jd)
+    
+    def dir_op(self, x):
+        y = self.op.forward(x) 
+        return y
+    
+    def adj_op(self, y):
+        x = self.op.adjoint(y)
+        return x
+    
+    def self_adj(self, x):
+        y = self.dir_op(x)
+        x = self.adj_op(y)
+        return x
+
+
+
+def RadialSampling(n_spokes=37, n_bins=10):
+    """Returns the u,v coordinates for sampling on radial spokes. Normalised to [-pi, pi]"""
+    r   = np.linspace(0, np.pi,   n_bins, endpoint=False)
+    phi = np.linspace(0, 2*np.pi, n_spokes,   endpoint=False)
+
+    rr, pp = np.meshgrid(r, phi)
+    
+    vis = np.array([(rr*np.sin(pp)).reshape(-1), (rr*np.cos(pp)).reshape(-1)]).T
+    return vis
+
+
+def SpiderSampling(n_spokes=37, n_lenslets=24, spectral_bins=10, max_baseline=0.65, min_baseline=0.15):
+    """Returns the u,v coordinates for sampling following the SPIDER instrument. Normalised to [-pi, pi]"""
+
+    coordinates = np.array([ ( (lens+1)/n_lenslets * (max_baseline-min_baseline) + min_baseline)* np.array([np.cos(2*np.pi *spoke/n_spokes), np.sin(2*np.pi *spoke/n_spokes)]) for spoke in range(n_spokes) for lens in range(n_lenslets)])
+
+    vis = list()
+    for n in range(n_spokes):
+        for i in range(n_lenslets//2):
+            x1 = coordinates[n*n_lenslets + i]
+            x2 = coordinates[(n+1)*n_lenslets -i -1]
+            for k in range(spectral_bins):
+                vis.append(2*np.pi*(x1-x2)/(500e-9 + (900e-9-500e-9)*(k/(spectral_bins - 1)) )) 
+
+    vis = np.array(vis)
+    vis = vis / np.max(np.sqrt(vis[:,0]**2 + vis[:,1]**2)) * np.pi
+    return vis
+
+
+def solver(y, phi=None):
+
+    x_init = phi.adj_op(y)
+
+    options = {'tol': 10e-5, 'iter': 5000, 'update_iter': 50, 'record_iters': False}
+    ISNR = 20.
+    sigma = 10**(-ISNR / 20.)
+    size = len(y)
+
+    epsilon = np.sqrt(size + 2. * np.sqrt(size)) * sigma
+
+    # proximal operator
+    p = prox_operators.l2_ball( epsilon, y, phi)
+    nu, sol = linear_operators.power_method(phi, np.ones_like(x_init))
+    p.beta = nu
+
+    # sparsity 
+    wav = ['db' + str(i) for i in range(1,8)]
+    levels = 4
+    shape = (size,)
+    psi = linear_operators.dictionary(wav, levels, x_init.shape)
+
+    h = prox_operators.l1_norm(np.max(np.abs(psi.dir_op(x_init))) * 1e-3, psi)
+    h.beta = 1.
+
+    # f = prox_operators.real_prox()
+    f = None
+
+
+    z, diag = primal_dual.FBPD(x_init, options, None, f, h, p)
+
+    return z
