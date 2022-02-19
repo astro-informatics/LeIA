a4bbde28a6c74a70b02420f668e1887a9f16dd86
Matthijs Mars <mars@hypatia-login.local>
2022-02-19 14:45:42 +0000

    Fix: changed operators such that np and TF implemenations are equivalent. Added test to make sure this is achieved

--- src/operators/NNFFT2D.py
+++ src/operators/NNFFT2D.py
@@ -1,46 +1,87 @@
+import tqdm
+import sparse
 import numpy as np
 
 class NNFFT2D():
     """Nearest Neighbour FFT. This operator maps the u,v coordinates to the nearest gridpoint and does an FFT. """
-    def plan(self, vis, Nd=(256,256), Kd=(512,512), Jd=None):
-        """ Initialises the measurement operators for u,v coordinates 'vis' (M x 2)"""
-
+    def plan(self, uv, Nd=(256,256), Kd=(512,512), Jd=None):
+    
+        # saving some values
         self.Nd = Nd
         self.Kd = Kd
-        self.vis = np.zeros(self.Kd, dtype=bool) #visability in 2d
+        self.Jd = Jd
+        self.n_measurements = len(uv)
 
-        for u,v in vis:
-            self.vis[ int(u/np.pi*Kd[0]//2 -.01)+Kd[0]//2, int(v/np.pi*Kd[1]//2-.01) + Kd[1]//2] = 1
+        gridsize = 2*np.pi / Kd[0]
+        k = (uv + np.pi) / gridsize
         
-        self.n_measurements = int(np.sum(vis))
+        # calculating coefficients for interpolation
+        indices = []
+        values =  []
+        for i in tqdm.tqdm(range(len(uv))):
+            ind, vals = (0, i, int(k[i,0]+.5), int(k[i,1] + 0.5)), 1
+            indices.append(ind)
+            values.append(vals.real)
+        
+        # building sparse matrix
+        indices = np.array(indices).reshape(-1, 4)
+        values = np.array(values).reshape(-1)
+        
+        #check if indices are within bounds, otherwise suppress them and raise warning
+        # TODO check in both dimensions instead of assuming square
+        if np.any(indices[:,2:] < 0) or np.any(indices[:,2:] >= Kd[0]):
+            sel_out_bounds = (np.any(indices[:,2:] < 0, axis=1) | np.any(indices[:,2:] >= Kd[0], axis=1))
+            print(f"some values lie out of the interpolation array, these are not used, check baselines")
+            indices = indices[~sel_out_bounds]
+            values = values[~sel_out_bounds]
+            
+        self.interp_matrix = sparse.COO(indices.T, values, shape=(1, len(uv), Kd[0], Kd[1]))
 
-    def dir_op(self, x):
-        # zero pad in real space for upsampled k-space
-        y = self._fft(np.pad(x, (
+    def dir_op(self, xx):
+        if xx.ndim == 2:
+            xx = xx[np.newaxis, :]
+        return np.squeeze(self._kk2k(self._xx2kk(self._pad((xx).reshape(-1, self.Nd[0], self.Nd[1])))))  # why divide on both sides?
+
+    def adj_op(self, k):
+        if k.ndim == 1:
+            k = k[np.newaxis, :]
+        kk = self._k2kk(k)
+        xx = self._kk2xx(kk)
+        xx = self._unpad(xx)
+        xx = np.squeeze(xx) 
+        return xx 
+        # return np.squeeze(self._unpad(self._kk2xx(self._k2kk(k)))) / self.scaling
+    
+    def _kk2k(self, kk):
+        """interpolates of the grid to non uniform measurements"""
+        return (self.interp_matrix * kk.reshape(-1, 1, self.Kd[0], self.Kd[1])).sum(axis=(2,3)).todense()
+    
+    def _k2kk(self, y):
+        """convolutes measurements to oversampled fft grid"""
+        return (self.interp_matrix * y.reshape(-1, self.n_measurements, 1, 1)).sum(axis=1).todense()
+    
+    @staticmethod
+    def _kk2xx(kk):
+        """from 2d fourier space to 2d image space"""
+        return np.fft.ifftshift(np.fft.ifft2(np.fft.ifftshift(kk, axes=(-2,-1)), axes=(-2,-1), norm='ortho'), axes=(-2,-1))
+
+    @staticmethod
+    def _xx2kk(xx):
+        """from 2d fourier space to 2d image space"""
+        return np.fft.fftshift(np.fft.fft2(np.fft.fftshift(xx, axes=(-2,-1)), axes=(-2,-1), norm='ortho'), axes=(-2,-1))
+    
+    def _pad(self, x):
+        """pads x to go from Nd to Kd"""
+        return np.pad(x, (
+            ( 0,0 ),
             ( (self.Kd[0]-self.Nd[0])//2, (self.Kd[0]-self.Nd[0])//2),
             ( (self.Kd[1]-self.Nd[1])//2, (self.Kd[1]-self.Nd[1])//2)
-            )))  # measure only the given visabilities
-        return y[self.vis]
+            ))
     
-    def adj_op(self, y):
-        y_ = np.zeros(self.Kd, dtype=complex)
-        y_[self.vis] = y
-        x = self._ifft(y_) # use only given visabilities for reconstruction
-        x = x[
+    def _unpad(self, x):
+        """unpads x to go from  Kd to Nd"""
+        return x[
+            :,
             (self.Kd[0]-self.Nd[0])//2: (self.Kd[0]-self.Nd[0])//2 +self.Nd[0],
             (self.Kd[1]-self.Nd[1])//2: (self.Kd[1]-self.Nd[1])//2 +self.Nd[1]
             ] # remove zero padding from image
-        return x
-    
-    @staticmethod
-    def _fft(x):
-        return np.fft.fftshift(np.fft.fft2(np.fft.fftshift(x), norm='ortho'))
-
-    @staticmethod
-    def _ifft(Fx):
-        return np.fft.fftshift(np.fft.ifft2(np.fft.fftshift(Fx), norm='ortho'))
-
-    def self_adj(self, x):
-        y = self.dir_op(x)
-        x = self.adj_op(y)
-        return x
--- src/tests/test_operators.py
+++ src/tests/test_operators.py
@@ -1,3 +1,5 @@
+import pytest 
+
 import numpy as np
 import tensorflow as tf
 
@@ -7,7 +9,6 @@ from src.operators.NUFFT2D_TF import NUFFT2D_TF
 from src.operators.NNFFT2D import NNFFT2D
 from src.operators.NNFFT2D_TF import NNFFT2D_TF
 
-np.random.seed(42) # some random sampling patterns do not work so fixed pattern
 
 def dot_product_test_np(op, shape_x, shape_y):
     x = np.random.normal(size=shape_x)
@@ -37,42 +38,74 @@ def dot_product_test_TF(op, shape_x, shape_y):
     b = np.sum(np.conjugate(x_.numpy()) * x.numpy())
     assert np.allclose(a, b)
 
-def test_self_adjointness_NUFFT2D():
-    shape_x = (32,32)
-    shape_y = (200,)
+@pytest.fixture()
+def shape_x():
+    return (32,32)
+
+@pytest.fixture()
+def shape_y():
+    return((200,))
+
+@pytest.fixture()
+def uv(shape_y):
+    np.random.seed(38204) # some random sampling patterns do not work so fixed pattern
     uv = np.random.normal(size=(shape_y[0], 2))
     uv /= uv.max() * np.pi
+    return uv
+
+@pytest.fixture()
+def NUFFT2D_op(uv, shape_x):
     op = NUFFT2D()
     op.plan(uv, Nd=shape_x, Kd=(64,64), Jd=(3,3))
+    return op
 
-    dot_product_test_np(op, shape_x=shape_x, shape_y=shape_y)
-
-def test_self_adjointness_NUFFT2D_TF():
-    shape_x = (32,32)
-    shape_y = (200,)
-    uv = np.random.normal(size=(shape_y[0], 2))
-    uv /= uv.max() * np.pi
+@pytest.fixture()
+def NUFFT2D_TF_op(uv, shape_x):
     op = NUFFT2D_TF()
     op.plan(uv, Nd=shape_x, Kd=(64,64), Jd=(3,3), batch_size=1)
+    return op
 
-    dot_product_test_TF(op, shape_x=shape_x, shape_y=shape_y)
-
-def test_self_adjointness_NNFFT2D():
-    shape_x = (32,32)
-    shape_y = (200,)
-    uv = np.random.normal(size=(shape_y[0], 2))
-    uv /= uv.max() * np.pi
+@pytest.fixture()
+def NNFFT2D_op(uv, shape_x):
     op = NNFFT2D()
     op.plan(uv, Nd=shape_x, Kd=(64,64), Jd=(3,3))
+    return op
 
-    dot_product_test_np(op, shape_x=shape_x, shape_y=shape_y)
-
-def test_self_adjointness_NNFFT2D_TF():
-    shape_x = (32,32)
-    shape_y = (200,)
-    uv = np.random.normal(size=(shape_y[0], 2))
-    uv /= uv.max() * np.pi
+@pytest.fixture()
+def NNFFT2D_TF_op(uv, shape_x):
     op = NNFFT2D_TF()
     op.plan(uv, Nd=shape_x, Kd=(64,64), Jd=(3,3), batch_size=1)
+    return op
 
-    dot_product_test_TF(op, shape_x=shape_x, shape_y=shape_y)
+def test_self_adjointness_NUFFT2D(NUFFT2D_op, shape_x, shape_y):
+    dot_product_test_np(NUFFT2D_op, shape_x=shape_x, shape_y=shape_y)
+
+def test_self_adjointness_NUFFT2D_TF(NUFFT2D_TF_op, shape_x, shape_y):
+    dot_product_test_TF(NUFFT2D_TF_op, shape_x=shape_x, shape_y=shape_y)
+
+def test_self_adjointness_NNFFT2D(NNFFT2D_op, shape_x, shape_y):
+    dot_product_test_np(NNFFT2D_op, shape_x=shape_x, shape_y=shape_y)
+
+def test_self_adjointness_NNFFT2D_TF(NNFFT2D_TF_op, shape_x, shape_y):
+    dot_product_test_TF(NNFFT2D_TF_op, shape_x=shape_x, shape_y=shape_y)
+
+def test_np_vs_TF(shape_x, shape_y, NUFFT2D_op, NUFFT2D_TF_op, NNFFT2D_op, NNFFT2D_TF_op):
+    x = np.random.normal(size=shape_x)[np.newaxis, :]
+    x_TF = tf.convert_to_tensor(x, dtype=tf.complex64)
+
+    y = np.random.normal(size=shape_y)[np.newaxis, :] \
+        + 1j * np.random.normal(size=shape_y)[np.newaxis, :] 
+    y_TF = tf.convert_to_tensor(y, dtype=tf.complex64)
+
+    assert np.allclose(NUFFT2D_op.dir_op(x), NUFFT2D_TF_op.dir_op(x_TF).numpy()), "nufft forward not the same"
+    assert np.allclose(NUFFT2D_op.adj_op(y), NUFFT2D_TF_op.adj_op(y_TF).numpy()), "nufft adjoint not the same"
+
+
+    y = np.random.normal(size=NNFFT2D_op.n_measurements)[np.newaxis, :] \
+        + 1j * np.random.normal(size=NNFFT2D_op.n_measurements)[np.newaxis, :] 
+    y_TF = tf.convert_to_tensor(y, dtype=tf.complex64)
+
+    assert np.allclose(NNFFT2D_op.dir_op(x), NNFFT2D_TF_op.dir_op(x_TF).numpy()), "nufft forward not the same"
+    assert np.allclose(NNFFT2D_op.adj_op(y), NNFFT2D_TF_op.adj_op(y_TF).numpy()), "nufft adjoint not the same"
+
+    # NNFFT2D_op, NNFFT2D_TF_op
