920f6b81fc1f275784331c0fde6b112a9147f2f3
Matthijs Mars <mars@hypatia-login.local>
2022-03-04 15:45:55 +0000

    Add version numbers to requirements, fix network test

--- src/tests/test_networks.py
+++ src/tests/test_networks.py
@@ -1,12 +1,17 @@
 import numpy as np
+import tensorflow as tf
 
-from src.networks.UNet import UNet
 from src.operators.NUFFT2D_TF import NUFFT2D_TF
+from src.networks.UNet import UNet
+from src.networks.GUnet import GUnet
+from src.networks.GUnet_variant import GUnet_variant2
 from src.networks.HighLowPassNet import HighLowPassNet
+from src.networks.LFB import LFB
 
 from src.sampling.uv_sampling import random_sampling
 
 def test_UNet():
+    tf.compat.v1.disable_eager_execution()
     shape = (32,32)
 
     uv = np.random.normal(size=(200,2))
@@ -21,6 +26,7 @@ def test_UNet():
 # test_Unet()
 
 def test_highlow():
+    tf.compat.v1.disable_eager_execution()
     shape = (32,32)
     # print(m.summary())
 
@@ -35,6 +41,7 @@ def test_highlow():
 
 
 def test_GUNet():
+    tf.compat.v1.disable_eager_execution()
     shape = (32,32)
     
     uv = random_sampling(200)
@@ -42,5 +49,29 @@ def test_GUNet():
     measurement_weights = np.linalg.norm(uv, axis=1)
     measurement_weights /= measurement_weights.max()
 
-    m = HighLowPassNet(shape, uv=uv, op=NUFFT2D_TF, measurement_weights=measurement_weights)
+    m = GUnet(shape, uv=uv, op=NUFFT2D_TF, measurement_weights=measurement_weights)
+    # print(m.summary())
+
+def test_GUNet_variant():
+    tf.compat.v1.disable_eager_execution()
+    shape = (32,32)
+    
+    uv = random_sampling(200)
+
+    measurement_weights = np.linalg.norm(uv, axis=1)
+    measurement_weights /= measurement_weights.max()
+
+    m = GUnet_variant.GUnet_variant2(shape, uv=uv, op=NUFFT2D_TF, measurement_weights=measurement_weights)
+    # print(m.summary())
+
+def test_LFB():
+    tf.compat.v1.disable_eager_execution()
+    shape = (32,32)
+    
+    uv = random_sampling(200)
+
+    measurement_weights = np.linalg.norm(uv, axis=1)
+    measurement_weights /= measurement_weights.max()
+
+    m = LFB(shape, uv=uv, op=NUFFT2D_TF, measurement_weights=measurement_weights)
     # print(m.summary())
