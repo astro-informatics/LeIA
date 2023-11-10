6a5e1de1fd14a696bb99f7b853073d1d7a1bd050
Matthijs <matthijs.mars97@gmail.com>
2023-11-10 20:07:23 +0000

    Remove nnfft operators from init and tests

--- src/operators/__init__.py
+++ src/operators/__init__.py
@@ -1,9 +1,4 @@
 from . import NUFFT2D
 from . import NUFFT2D_TF
-
-from . import NNFFT2D
-from . import NNFFT2D_TF
-
 from . import IdentityOperator
-
 from . import kernels
--- src/tests/test_operators.py
+++ src/tests/test_operators.py
@@ -6,8 +6,6 @@ import tensorflow as tf
 from src.operators.NUFFT2D import NUFFT2D
 from src.operators.NUFFT2D_TF import NUFFT2D_TF
 
-from src.operators.NNFFT2D import NNFFT2D
-from src.operators.NNFFT2D_TF import NNFFT2D_TF
 
 
 def dot_product_test_np(op, shape_x, shape_y):
@@ -67,17 +65,6 @@ def NUFFT2D_TF_op(uv, shape_x):
     op.plan(uv, Nd=shape_x[1:], Kd=(64,64), Jd=(6,6), batch_size=2)
     return op
 
-@pytest.fixture()
-def NNFFT2D_op(uv, shape_x):
-    op = NNFFT2D()
-    op.plan(uv, Nd=shape_x[1:], Kd=(64,64), Jd=(3,3))
-    return op
-
-@pytest.fixture()
-def NNFFT2D_TF_op(uv, shape_x):
-    op = NNFFT2D_TF()
-    op.plan(uv, Nd=shape_x[1:], Kd=(64,64), Jd=(3,3), batch_size=2)
-    return op
 
 def test_self_adjointness_NUFFT2D(NUFFT2D_op, shape_x, shape_y):
     dot_product_test_np(NUFFT2D_op, shape_x=shape_x, shape_y=shape_y)
@@ -85,11 +72,6 @@ def test_self_adjointness_NUFFT2D(NUFFT2D_op, shape_x, shape_y):
 def test_self_adjointness_NUFFT2D_TF(NUFFT2D_TF_op, shape_x, shape_y):
     dot_product_test_TF(NUFFT2D_TF_op, shape_x=shape_x, shape_y=shape_y)
 
-# def test_self_adjointness_NNFFT2D(NNFFT2D_op, shape_x, shape_y):
-#     dot_product_test_np(NNFFT2D_op, shape_x=shape_x, shape_y=shape_y)
-
-# def test_self_adjointness_NNFFT2D_TF(NNFFT2D_TF_op, shape_x, shape_y):
-#     dot_product_test_TF(NNFFT2D_TF_op, shape_x=shape_x, shape_y=shape_y)
 
 def test_np_vs_TF(shape_x, shape_y, NUFFT2D_op, NUFFT2D_TF_op, NNFFT2D_op, NNFFT2D_TF_op):
     assert tf.executing_eagerly()
@@ -102,10 +84,3 @@ def test_np_vs_TF(shape_x, shape_y, NUFFT2D_op, NUFFT2D_TF_op, NNFFT2D_op, NNFFT
 
     tf.debugging.assert_near(NUFFT2D_op.dir_op(x).astype(np.complex64), NUFFT2D_TF_op.dir_op(x_TF))
     tf.debugging.assert_near(NUFFT2D_op.adj_op(y).astype(np.complex64), NUFFT2D_TF_op.adj_op(y_TF))
-
-    # y = np.random.normal(size=NNFFT2D_op.n_measurements)[np.newaxis, :] \
-    #     + 1j * np.random.normal(size=NNFFT2D_op.n_measurements)[np.newaxis, :]
-    # y_TF = tf.convert_to_tensor(y, dtype=tf.complex64)
-
-    # tf.debugging.assert_near(NNFFT2D_op.dir_op(x).astype(np.complex64), NNFFT2D_TF_op.dir_op(x_TF))
-    # tf.debugging.assert_near(NNFFT2D_op.adj_op(y).astype(np.complex64), NNFFT2D_TF_op.adj_op(y_TF))
